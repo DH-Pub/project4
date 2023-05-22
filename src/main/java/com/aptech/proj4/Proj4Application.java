9283ad714c61341f35347e030da18795be97a959
DH-Pub <68387587+DH-Pub@users.noreply.github.com>
2023-05-22 19:08:01 +0700

    run

--- src/main/java/com/aptech/proj4/Proj4Application.java
+++ src/main/java/com/aptech/proj4/Proj4Application.java
@@ -3,7 +3,12 @@ package com.aptech.proj4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "springdoc-openapi", version = "1.0.0"), security = {@SecurityRequirement(name = "bearer-key")})
 public class Proj4Application {
 
 	public static void main(String[] args) {
