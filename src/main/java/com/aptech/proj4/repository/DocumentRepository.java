7c44e06d6527bc103ed0c36e2d4a16e4c0bfdf84
lavisar <sarlavi6969@gmail.com>
2023-06-16 09:01:10 +0700

    chore: add DocumentRepository

---
+++ src/main/java/com/aptech/proj4/repository/DocumentRepository.java
@@ -0,0 +1,11 @@
+package com.aptech.proj4.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.aptech.proj4.model.Document;
+
+public interface DocumentRepository extends CrudRepository<Document, String> {
+    Optional<Document> findByName(String name);
+}
