92277dadb72009be89b6f8453e0dfe61869f8d1d
lavisar <sarlavi6969@gmail.com>
2023-06-26 08:34:41 +0700

    feat (comment API): finished

---
+++ src/main/java/com/aptech/proj4/controller/CommentController.java
@@ -0,0 +1,59 @@
+package com.aptech.proj4.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aptech.proj4.dto.CommentDto;
+import com.aptech.proj4.model.Comment;
+import com.aptech.proj4.service.CommentService;
+
+@RestController
+@RequestMapping("/comment")
+public class CommentController {
+  @Autowired
+  private CommentService commentService;
+
+  @PostMapping("/create")
+  public ResponseEntity<?> createdComment(@RequestBody CommentDto commentDto, @RequestParam String taskId,
+      @RequestParam String userId) {
+    CommentDto savedComment = commentService.createComment(commentDto, taskId, userId);
+    return ResponseEntity.ok(savedComment);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<CommentDto> updateComment(@PathVariable String id, @RequestBody CommentDto commentDto) {
+    CommentDto updatedComment = commentService.updateComment(id, commentDto);
+    if (updatedComment != null) {
+      return ResponseEntity.ok(updatedComment);
+    }
+    return ResponseEntity.notFound().build();
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<String> deleteComment(@PathVariable("id") String id) {
+    boolean deleted = commentService.deleteComment(id);
+    if (deleted) {
+      return ResponseEntity.status(HttpStatus.OK).body("Comment deleted successfully");
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  @GetMapping
+  public ResponseEntity<List<Comment>> getAllComments() {
+    List<Comment> comments = commentService.getAllComments();
+    return ResponseEntity.ok(comments);
+  }
+}
--- src/main/java/com/aptech/proj4/service/CommentServiceImpl.java
+++ src/main/java/com/aptech/proj4/service/CommentServiceImpl.java
@@ -1,7 +1,9 @@
 package com.aptech.proj4.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,22 +12,47 @@ import org.springframework.stereotype.Service;
 import com.aptech.proj4.dto.CommentDto;
 import com.aptech.proj4.dto.UserDto;
 import com.aptech.proj4.model.Comment;
+import com.aptech.proj4.model.Task;
+import com.aptech.proj4.model.User;
 import com.aptech.proj4.repository.CommentRepository;
+import com.aptech.proj4.repository.TaskRepository;
+import com.aptech.proj4.repository.UserRepository;
 
 @Service
 public class CommentServiceImpl implements CommentService {
   @Autowired
   CommentRepository commentRepository;
   @Autowired
+  TaskRepository taskRepository;
+  @Autowired
+  UserRepository userRepository;
+  @Autowired
   private ModelMapper modelMapper;
 
-  // @Override
-  // public CommentDto createComment(CommentDto comment, TaskDto task, UserDto
-  // user) {
-  // // TODO Auto-generated method stub
-  // throw new UnsupportedOperationException("Unimplemented method
-  // 'createComment'");
-  // }
+  @Override
+  public CommentDto createComment(CommentDto commentDto, String taskId, String userId) {
+    Task task = taskRepository.findById(taskId)
+        .orElseThrow(() -> new NoSuchElementException("Task ID not found"));
+
+    User user = userRepository.findById(userId)
+        .orElseThrow(() -> new NoSuchElementException("User ID not found"));
+
+    Comment comment = new Comment()
+        .setId(Long.toString(System.currentTimeMillis()))
+        .setTaskId(task)
+        .setComment(commentDto.getComment())
+        .setCreatedBy(user);
+
+    Comment savedComment = commentRepository.save(comment);
+
+    CommentDto savedCommentDto = new CommentDto();
+    savedCommentDto.setId(savedComment.getId());
+    savedCommentDto.setTaskId(savedComment.getTaskId().getId());
+    savedCommentDto.setComment(savedComment.getComment());
+    savedCommentDto.setCreatedBy(savedComment.getCreatedBy().getId());
+
+    return savedCommentDto;
+  }
 
   @Override
   public boolean deleteComment(String id) {
@@ -36,10 +63,15 @@ public class CommentServiceImpl implements CommentService {
   }
 
   @Override
-  public CommentDto updateComment(CommentDto comment) {
-    Comment updatedComment = modelMapper.map(comment, Comment.class);
-    commentRepository.save(updatedComment);
-    return comment;
+  public CommentDto updateComment(String id, CommentDto commentDto) {
+    Comment existingComment = commentRepository.findById(id)
+        .orElseThrow(() -> new NoSuchElementException("Comment ID not found"));
+
+    existingComment.setComment(commentDto.getComment());
+    Comment updatedComment = commentRepository.save(existingComment);
+
+    CommentDto updatedCommentDto = modelMapper.map(updatedComment, CommentDto.class);
+    return updatedCommentDto;
   }
 
   @Override
