efa10a52ae7db3fcac118792f2d1c658f810fa19
QUOCTRUNG14\lequo <lequoctrung1407@gmail.com>
2023-07-13 01:00:58 +0700

    get task by user API

--- src/main/java/com/aptech/proj4/repository/TaskRepository.java
+++ src/main/java/com/aptech/proj4/repository/TaskRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.aptech.proj4.model.Project;
 import com.aptech.proj4.model.Task;
+import com.aptech.proj4.model.User;
 
 public interface TaskRepository extends CrudRepository<Task, String>  {
     List<Task> findByProject(Project project);
+    List<Task> findByUser(User user);
 }
